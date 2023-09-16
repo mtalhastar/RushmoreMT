829085f3c018f2006826d4fbcb0d2759455c3687
mtalhastar <mtalhastar@gmail.com>
2023-09-16 15:06:46 +0500

    done

--- lib/screens/resultPage.dart
+++ lib/screens/resultPage.dart
@@ -17,23 +17,108 @@ class ResultScreen extends StatelessWidget {
                 child: Column(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
+                      const SizedBox(
+                        height: 80,
+                      ),
                       CardWidgetTwo(
                         image1: HomeController.instance.imagesList[0],
                         image2: HomeController.instance.imagesList[1],
                         image3: HomeController.instance.imagesList[2],
                         image4: HomeController.instance.imagesList[3],
                       ),
-                      SizedBox(
-                        height: 40,
+                      const SizedBox(
+                        height: 30,
                       ),
                       const Text(
-                        'Type Celebrity Names',
+                        'Congratulations!',
                         textAlign: TextAlign.center,
                         style: TextStyle(
                           color: Colors.black,
                           fontSize: 24,
                           fontFamily: 'Inter',
                           fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      const Text(
+                        'Your Mount Rushmore is ready.\n Share it with your friends',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w300,
+                          height: 0,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Container(
+                        width: 284,
+                        height: 57,
+                        alignment: Alignment.center,
+                        decoration: ShapeDecoration(
+                          color: Colors.black,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                        ),
+                        child: const Text(
+                          'Share',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.w600,
+                            height: 0,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      const Text(
+                        'OR',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w300,
+                          height: 0,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      InkWell(
+                        onTap: () {
+                          
+                        },
+                        child: Container(
+                          width: 284,
+                          height: 57,
+                          alignment: Alignment.center,
+                          decoration: ShapeDecoration(
+                            color: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              side: BorderSide(width: 1),
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                          ),
+                          child: const Text(
+                            'Try Again',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 24,
+                              fontFamily: 'Inter',
+                              fontWeight: FontWeight.w600,
+                              height: 0,
+                            ),
+                          ),
                         ),
                       ),
                     ]))));
